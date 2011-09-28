14a8c07d369ee90519eaee6e40c7d130a17a1eee
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-09-28 18:36:48 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c931d36..53af70b 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension File::Zglob
    
    +0.06
    +
    +    - '0' is true value.
    +    - optimization
    +
     0.05
    
         - support ~foo/

--- lib/File/Zglob.pm
+++ lib/File/Zglob.pm
@@ -2,7 +2,7 @@ package File::Zglob;
 use strict;
 use warnings FATAL => 'recursion';
 use 5.008008;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use base qw(Exporter);
 
 our @EXPORT = qw(zglob);
