431725992070141dae30cfdd6f720b4249f5905c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-09-28 17:02:41 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 3e6f7ee..749ce0f 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension File::Zglob
    
    +0.04
    +
    +    - pod fix
    +
     0.03
    
         - doc fix

--- lib/File/Zglob.pm
+++ lib/File/Zglob.pm
@@ -2,7 +2,7 @@ package File::Zglob;
 use strict;
 use warnings FATAL => 'recursion';
 use 5.008008;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 use base qw(Exporter);
 
 our @EXPORT = qw(zglob);
